c20cfb399c3a09c80c60fb24d5ea4d8dba5b8591
MSI\user <michal.potoczny.92@gmail.com>
2022-12-28 14:28:28 +0100

    Change hero section

--- front-page.php
+++ front-page.php
@@ -17,35 +17,15 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<section class="container pb-5 pt-4">
-			<div id="carouselExampleDark" class="carousel carousel-dark slide overflow-hidden rounded" data-bs-ride="carousel">
-				<div class="carousel-indicators">
-					<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-					<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-				</div>
-				<div class="carousel-inner">
-					<div class="carousel-item active" data-bs-interval="10000">
-						<a href="#"><img src="<?php echo get_template_directory_uri();?>/img/slider/slide-1.jpg" class="d-block w-100" alt="..."></a>
-					</div>
-					<div class="carousel-item" data-bs-interval="2000">
-						<a href="#"><img src="<?php echo get_template_directory_uri();?>/img/slider/slide-2.jpg" class="d-block w-100" alt="..."></a>
-					</div>
-				</div>
-				<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"  data-bs-slide="prev">
-					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span class="visually-hidden">Previous</span>
-				</button>
-				<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"  data-bs-slide="next">
-					<span class="carousel-control-next-icon" aria-hidden="true"></span>
-					<span class="visually-hidden">Next</span>
-				</button>
-			</div>
+		<section class="hero container pb-5 pt-4">
+			<h1>Czym jest Flufffy.pl?</h1>
+			<div>To duże, ciepłe koce, które możesz, a nawet musisz nosić, aby poczuć się komfortowo i ciepło w chłodne, zimowe wieczory</div>
 		</section>
 
 
 		<section class="container popular-products">
-			<h1 class="text-center pt-5">Popular Products</h1>
-			<p class="text-center">We offer a number of high quality toys to help keep<br> your pets healthy and spoiled!</p>
+			<h1 class="text-center pt-5">Polecamy</h1>
+			<p class="text-center">Miękkie i ciepłe kocopiżamy</p>
 
 			<div class="pt-5 pb-5">
 				<?php echo do_shortcode( '[products popularity columns=4 limit=4]' ); ?>
